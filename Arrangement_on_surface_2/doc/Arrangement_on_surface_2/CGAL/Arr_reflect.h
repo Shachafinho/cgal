7ce820de5f3619547f22c5306df8df654e84aabf
Efi Fogel <efifogel@gmail.com>
2017-07-23 11:47:52 +0300

    1st revision

---
+++ Arrangement_on_surface_2/doc/Arrangement_on_surface_2/CGAL/Arr_reflect.h
@@ -0,0 +1,24 @@
+namespace CGAL {
+
+/*! \ingroup PkgArrangement2Funcs
+ *
+ * \brief Computes the reflection of an arrangement through a point.
+ * \details Computes the reflection of an arrangement through a point.
+ */
+template <typename GeomeTraits, typename GeomeTraitsRes
+          typename TopolTraits, typename TopolTraitsRes>
+void reflect(const Arrangement_on_surface_2<GeomeTraits, TopolTraits>& arr,
+             Arrangement_on_surface_2<GeomeTraits, TopolTraits>& arr_res,
+             const typename GeomeTraits::Point_2& p);
+
+/*! \ingroup PkgArrangement2Funcs
+ *
+ * \brief Computes the reflection of an arrangement through the origin.
+ * \details Computes the reflection of an arrangement through the origin.
+ */
+template <typename GeomeTraits, typename GeomeTraitsRes
+          typename TopolTraits, typename TopolTraitsRes>
+void reflect(const Arrangement_on_surface_2<GeomeTraits, TopolTraits>& arr,
+             Arrangement_on_surface_2<GeomeTraits, TopolTraits>& arr_res);
+
+} // namesapce CGAL
