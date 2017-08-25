ae4f9cecb47337918f0582345f3666834568f1c8
Shachaf Ben Jakov <shachafinho@gmail.com>
2017-08-25 21:43:47 +0300

    Compile-time check for supported traits.
    Added a compile-time check to ensure we only reflect supported traits. Calling reflect() with unsupported traits will generate an error message at run-time.

--- Arrangement_on_surface_2/include/CGAL/Arr_reflect.h
+++ Arrangement_on_surface_2/include/CGAL/Arr_reflect.h
@@ -44,14 +44,34 @@
 namespace CGAL {
 
 /*!
- * Computes the reflection of an arrangement through the origin.
- * \param arr The arrangement to reflect.
- * \param arr_res Output: The resulting arrangement.
+ * Determines at compile-time whether T has a nested type named Reflect_2.
+ * If so, 'value' holds true, and false otherwise.
+ */
+template <typename T>
+struct has_Reflect_2
+{
+  // The following types must differ in size.
+  typedef char yes[1];
+  typedef char no[2];
+  
+  // Overloads for when U does or doesn't have the Reflect_2 functor, respectively.
+  template <typename U> static yes& check(typename U::Reflect_2 *);
+  template <typename U> static no& check(...);
+
+  // True iff the first overload worked and T has a nested type named Reflect_2.
+  static const bool value = sizeof(check<T>(nullptr)) == sizeof(yes);
+};
+
+
+/*!
+ * The following overload takes place if GeomeTraitsRes support reflection.
+ * It reflects the arrangement using the traits' Reflect_2 functor.
  */
 template <typename GeomeTraits, typename GeomeTraitsRes,
           typename TopolTraits, typename TopolTraitsRes>
-void reflect(const Arrangement_on_surface_2<GeomeTraits, TopolTraits>& arr,
-             Arrangement_on_surface_2<GeomeTraitsRes, TopolTraitsRes>& arr_res)
+typename boost::enable_if_c<has_Reflect_2<GeomeTraitsRes>::value, void>::type
+do_reflect(const Arrangement_on_surface_2<GeomeTraits, TopolTraits>& arr,
+                Arrangement_on_surface_2<GeomeTraitsRes, TopolTraitsRes>& arr_res)
 {
   // Copy the initial arrangement and reflect it in-place
 
@@ -69,8 +89,6 @@ void reflect(const Arrangement_on_surface_2<GeomeTraits, TopolTraits>& arr,
   ((boost::is_convertible<typename GeomeTraits::X_monotone_curve_2, \
                           typename GeomeTraitsRes::X_monotone_curve_2>::value));
 
-  // TODO: Check if GeomeTraitsRes has the reflect_2_object() method.
-
   // The result arrangement cannot be the input arrangement.
   CGAL_precondition((void *)(&arr_res) != (void *)(&arr));
   
@@ -84,6 +102,7 @@ void reflect(const Arrangement_on_surface_2<GeomeTraits, TopolTraits>& arr,
   // Obtain a reflect object to reflect the geometric objects
   Reflect_2 reflect_object = arr_res.geometry_traits()->reflect_2_object();
 
+
   // Reflect the point of each vertex through p
   for (Vertex_iterator vit = arr_res.vertices_begin(); vit != arr_res.vertices_end(); ++vit) {
     accessor.modify_vertex_ex(vit, reflect_object(vit->point()));
@@ -93,6 +112,32 @@ void reflect(const Arrangement_on_surface_2<GeomeTraits, TopolTraits>& arr,
   for (Edge_iterator eit = arr_res.edges_begin(); eit != arr_res.edges_end(); ++eit) {
     accessor.modify_edge_ex(eit, reflect_object(eit->curve()));
   }
+}
+
+/*!
+ * The following overload takes place if GeomeTraitsRes do not support reflection.
+ * Reports an appropriate error message.
+ */
+template <typename GeomeTraits, typename GeomeTraitsRes,
+          typename TopolTraits, typename TopolTraitsRes>
+typename boost::enable_if_c<!has_Reflect_2<GeomeTraitsRes>::value, void>::type
+  do_reflect(const Arrangement_on_surface_2<GeomeTraits, TopolTraits>& arr,
+                Arrangement_on_surface_2<GeomeTraitsRes, TopolTraitsRes>& arr_res)
+{
+  CGAL_error_msg("The result arrangement traits do not support reflection!");
+}
+
+/*!
+ * Computes the reflection of an arrangement through the origin.
+ * \param arr The arrangement to reflect.
+ * \param arr_res Output: The resulting arrangement.
+ */
+template <typename GeomeTraits, typename GeomeTraitsRes,
+          typename TopolTraits, typename TopolTraitsRes>
+void reflect(const Arrangement_on_surface_2<GeomeTraits, TopolTraits>& arr,
+             Arrangement_on_surface_2<GeomeTraitsRes, TopolTraitsRes>& arr_res)
+{
+  do_reflect(arr, arr_res);
 }
 } //namespace CGAL
 
